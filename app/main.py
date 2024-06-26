cf4848f62cf06f39b99ec50fd22596183853e353
Lucas Maciel <lucasmaciel63@gmail.com>
2024-06-26 08:31:22 -0300

    remove comments

--- app/main.py
+++ app/main.py
@@ -8,17 +8,12 @@ def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
 
-    # Inicializa o banco de dados com o Flask
     db.init_app(app)
     migrate = Migrate(app, db)
-
-    # Registra os blueprints
     register_blueprints(app)
-
-    # Configurações adicionais podem ser adicionadas aqui, como loggers, ferramentas de monitoramento, etc.
 
     return app
 
 if __name__ == "__main__":
     app = create_app()
-    app.run(debug=True)  # Lembre-se de desativar o modo debug em produção
+    app.run(debug=True)
