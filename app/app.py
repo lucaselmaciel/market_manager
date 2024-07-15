33c4f6c461266c16238032f0036b81c3de8a377e
Lucas Maciel <lucasmaciel63@gmail.com>
2024-07-15 10:29:05 -0300

    create db

--- app/app.py
+++ app/app.py
@@ -1,10 +1,12 @@
 from flask import Flask
-from app.config import Config
+from app.config import Config, DevelopmentConfig
 from flask_migrate import Migrate
 from app.routes.customer_routes import customer_bp
 from app.routes.product_routes import product_bp
 from app.routes.sale_routes import sale_bp
 from app import db
+import logging
+logging.basicConfig(level=logging.DEBUG)
 
 
 def register_blueprints(app):
@@ -25,5 +27,8 @@ def create_app(config_class=Config):
 
 
 if __name__ == "__main__":
-    app = create_app()
+    app = create_app(DevelopmentConfig)
+    handler = logging.StreamHandler()
+    handler.setLevel(logging.DEBUG)
+    app.logger.addHandler(handler)
     app.run(debug=True)
---
+++ app/schemas/__init__.py
@@ -0,0 +1,1 @@
+from app.schemas.sale_schemas import SaleSchema, SaleDetailSchema
