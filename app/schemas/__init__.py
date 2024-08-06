3e1cbb7fd698d79e59214fbaa8226cbaccf21202
Lucas Maciel <lucasmaciel63@gmail.com>
2024-08-05 21:09:58 -0300

    customer schemas changes

--- app/schemas/__init__.py
+++ app/schemas/__init__.py
@@ -1,2 +1,3 @@
 from app.schemas.sale_schemas import SaleSchema, SaleDetailSchema, SalesListFiltersSchema
 from app.schemas.product_schemas import ProductSchema
+from app.schemas.customer_schemas import CustomerSchema
--- app/schemas/customer_schemas.py
+++ app/schemas/customer_schemas.py
@@ -2,7 +2,7 @@ from marshmallow import Schema, fields, post_load
 from app.models.customer import Customer
 
 class CustomerSchema(Schema):
-    id = fields.Int(dump_only=True)
+    id = fields.Int()
     name = fields.Str(required=True)
     contact = fields.Str(required=True)
     address = fields.Str(required=True)
